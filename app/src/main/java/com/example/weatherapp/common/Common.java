6fe10c9985dab5673c08636eb73b5cba4a2694d0
dendyandra22 <dendyandra@gmail.com>
2020-03-29 20:12:27 +0700

    second commit

---
+++ app/src/main/java/com/example/weatherapp/common/Common.java
@@ -0,0 +1,5 @@
+package com.example.weatherapp.common;
+
+public class Common {
+    public  static final APP_ID = "832b3901bf48bd4cbb87d29ff511f5b1";
+}
---
+++ app/src/main/java/com/example/weatherapp/model/Clouds.java
@@ -0,0 +1,16 @@
+package com.example.weatherapp.model;
+
+class Clouds {
+    public int all;
+
+    public Clouds() {
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
